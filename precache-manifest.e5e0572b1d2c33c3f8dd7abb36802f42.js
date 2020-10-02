bde88b1fb514f62ed362ec77400ea602eb4c9548
Khushal <dev3.igeektech@gmail.com>
2020-10-02 18:10:10 +0530

    Updates

---
+++ precache-manifest.e5e0572b1d2c33c3f8dd7abb36802f42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "356c33933964077ae926f9a316aaa0f2",
+    "url": "/demo_qux/index.html"
+  },
+  {
+    "revision": "d6a391a7c13285344ec2",
+    "url": "/demo_qux/static/css/2.a32f9c7d.chunk.css"
+  },
+  {
+    "revision": "867d51277eb7a4bc8843",
+    "url": "/demo_qux/static/css/main.15f438e9.chunk.css"
+  },
+  {
+    "revision": "d6a391a7c13285344ec2",
+    "url": "/demo_qux/static/js/2.3a5d1353.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/demo_qux/static/js/2.3a5d1353.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "867d51277eb7a4bc8843",
+    "url": "/demo_qux/static/js/main.d45cd0db.chunk.js"
+  },
+  {
+    "revision": "05661cdac9f456dcb6ed",
+    "url": "/demo_qux/static/js/runtime-main.f139fb4f.js"
+  }
+]);
